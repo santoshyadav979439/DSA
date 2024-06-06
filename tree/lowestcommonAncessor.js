67393d52d57909eab9102992988d821db184407d
Santosh Yadav <49767268+santoshyadav979439@users.noreply.github.com>
2024-06-06 22:50:46 +0530

    Update lowestcommonAncessor.js

--- tree/lowestcommonAncessor.js
+++ tree/lowestcommonAncessor.js
@@ -1,3 +1,7 @@
+/*
+1. traverse the tree and find p and q, return true if you find p or q. if both subtree of node returns true then node is LCA. If node is 
+one of p or q then any of subtree will contain other node. hence node itself will be LCA.
+*/
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
     if (root == null || root === p || root === q) return root
     const left = lowestCommonAncestor(root.left,p,q)
